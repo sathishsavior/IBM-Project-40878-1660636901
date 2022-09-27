450aded3d19e7cab622dea3466045b4e675dd1f9
sathishsavior <112473263+sathishsavior@users.noreply.github.com>
2022-09-27 16:02:10 +0530

    Delete prime series.py

--- prime series.py
+++
@@ -1,9 +0,0 @@
-start=int(input("enter the starting range:"))
-end=int(input("enter the ending range:"))
-for num in range(start,end+1):
-    if num>1:
-        for i in range(2,num):
-            if (num%2)==0:
-                break
-            else:
-                print(num)
